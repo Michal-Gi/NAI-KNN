39cf12d739384002e26efe11aa118333d0585133
Michal-Gi <michalg394@gmail.com>
2023-03-14 23:33:43 +0100

    Added scanner

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("To rerun the K-NN algorithm enter the k value and a pair");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("To rerun the K-NN algorithm enter the k value and a pair. To terminate the program enter \"over\"");
+        String input = scanner.nextLine();
+
     }
 }
