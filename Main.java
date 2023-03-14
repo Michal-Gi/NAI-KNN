415a3b81b34dcf1309c50b788e3e9d2519bb393b
Michal-Gi <michalg394@gmail.com>
2023-03-14 19:34:32 +0100

    Created

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("To run the knn algorithm enter the k value and a pair");
+
+        System.out.println("To rerun the K-NN algorithm enter the k value and a pair");
     }
 }
